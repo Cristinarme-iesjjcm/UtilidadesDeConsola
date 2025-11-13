acd87c43bdfe5ebb8f2f77c0df3f1563b9eabc6d
Cristina <cristinarme@educastur.org>
2025-11-13 11:02:36 +0100

    Actualizo el saludo

--- src/ut2/Saludo.java
+++ src/ut2/Saludo.java
@@ -3,7 +3,7 @@ package ut2;
 public class Saludo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!");
+		System.out.println("Hola mundo, estoy conectado a GitHub!");
 
 	}
 
